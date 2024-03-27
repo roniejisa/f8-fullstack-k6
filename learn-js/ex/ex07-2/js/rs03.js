8c306c6efe8d3a9b072b5048930c3970ee5499f9
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-03-28 01:12:46 +0700

    update code

--- learn-js/ex/ex07-2/js/rs03.js
+++ learn-js/ex/ex07-2/js/rs03.js
@@ -1,6 +1,6 @@
 // Cho một mảng số nguyên chưa được sắp xếp nums. Hãy trả về số nguyên dương nhỏ nhất không có trong nums.
 
-var nums = [7, 8, 9, 11, 12];
+var nums = [9,6,5,4,3,2,1,-1,-3];
 
 
 
@@ -10,6 +10,9 @@ function test(nums) {
     for (var i = 0; i < nums.length; i++) {
         if (nums[i] !== 0 && nums[i] === minNumber) {
             minNumber++;
+        }
+        if (nums.indexOf(minNumber) === -1) {
+            break;
         }
     }
     return minNumber;
