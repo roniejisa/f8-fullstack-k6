497477dcc0518d61113d654da19f6bd9a9df6f44
Phạm Minh Hiếu <hieupm248@gmail.com>
2024-05-07 22:47:10 +0700

    update code

--- learn-js/ex/day41/src/utils/router.js
+++ learn-js/ex/day41/src/utils/router.js
@@ -1,6 +1,6 @@
 import Navigo from "navigo";
 import Error from "../pages/Error";
-const mainNavigo = new Navigo("/");
+const mainNavigo = new Navigo("/f8-fullstack-k6/learn-js/ex/day41/dist/");
 const app = document.querySelector("#app");
 
 export const router = (routes, defaultLayout) => {
